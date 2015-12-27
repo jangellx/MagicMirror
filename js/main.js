c9bcabc9914f769ea53d1f2ee124db86722de991
jangellx <jangell@tmproductions.com>
2015-12-27 14:51:38 -0500

    Really fixed "alert" vs. "alerts" this time

--- js/main.js
+++ js/main.js
@@ -1041,7 +1041,7 @@ jQuery(document).ready(function($) {
 				nonOngoingCount++;
 		}
 
-		var alertCountText = (mbtaAlerts.length == 1) ? 'alerts' : 'alert';
+		var alertCountText = (mbtaAlerts.length == 1) ? 'alert' : 'alerts';
 		if( nonOngoingCount > 0 ) {
 			// We have at least one alert to show
 			var	step = 0;
@@ -1063,7 +1063,7 @@ jQuery(document).ready(function($) {
 		// Update the div itself
 		$('.mbta').updateWithText(alerts, 1000);
 		$('.luMBTA').updateWithText('mbta (' + nonOngoingCount + '/'
-		                                     + mbtaAlerts.length + ' ' + ((nonOngoingCount == 1) ? 'alerts' : 'alert') + '): '
+		                                     + mbtaAlerts.length + ' ' + ((nonOngoingCount == 1) ? 'alert' : 'alerts') + '): '
 		                                     + moment().format('h:mm a ddd MMM D YYYY'), 1000);
 
 		// Rearm the timer for 5 minutes
